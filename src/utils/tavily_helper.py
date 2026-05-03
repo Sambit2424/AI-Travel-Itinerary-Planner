6e66f2245040c05d88f227639368d149e0f19e3c
sambit <sam@example.com>
2026-05-03 23:32:28 +0530

    pydantic logfire config done

---
+++ src/utils/tavily_helper.py
@@ -0,0 +1,4 @@
+# This module is extract structured info from tavily for ground data
+
+# Tavily -- top 5 results - 1000 tokens each is too verbose - we need to make it succinct
+# eval - ground data should be to the point
